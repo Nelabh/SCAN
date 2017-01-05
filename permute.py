8ebc12a25117ec0717527415fd8025fd792f2722
Nelabh Kotiya <nelabhkotiya@gmail.com>
2017-01-06 00:31:52 +0530

    Added Combination handling logic

--- permute.py
+++ permute.py
@@ -1,7 +1,6 @@
 from itertools import combinations
 def combination(list1,elements):
     r=1
-    print("Elements")
     while r <= len(list1):        
         for c in combinations(list1,r):
             ele = ""
@@ -9,26 +8,11 @@ def combination(list1,elements):
                 ele += c[i]
             elements.append(ele)
         r += 1
-    print(elements)
     return
 def permute(string1,string2,comparison):
-    #print("STRING 1 :")
-   # print(string1)
-    #print("STRING 2 :")
-    #print(string2)
-    #print("COMPARISON :")
-    #print(comparison)
-    
     list1 = []
     list2 = []
     common = []
-#    print("LIST 1 Before :")
- #   print(list1)
-  #  print("LIST 2BEFORE:")
-   # print(list2)
-   # print("COMMON Before :")
-   # print(common)
-    
     for index1 in range(len(string1)):
         if(comparison[ord(string1[index1])] == 1):
             list1.append(string1[index1])
@@ -38,30 +22,24 @@ def permute(string1,string2,comparison):
     for index2 in range(len(string2)):
         if(comparison[ord(string2[index2])] == 1):
             list2.append(string2[index2])           
-    #print("COMMON :")
-    #print(common)
-    #print("LIST 1 :")
-    #print(list1)
-    #print("LIST 2:")
-    #print(list2)
     comstr = ""
     elements = []
     elements2 = []
+    combi = []
     if common:
         for ind in range(len(common)):
             comstr += common[ind]
-    print("******LIST 1*****")
     if list1:
         combination(list1,elements)
-    print("******LIST 2*****")
     if list2:
          combination(list2,elements2)
-    print("ELEMENTS: ")
-    print(elements)
-    print("ELEMENTS 2: ")
-    print(elements2)
-   
-    return
+    for index in elements:
+        for index2 in elements2:
+            app = ''.join(sorted(comstr+index+index2))
+            if app not in combi:
+                combi.append(app)
+    combi = sorted(combi, key=lambda x: len(x))
+    return combi
 
 def lexico(string):
     return
--- scan1.py
+++ scan1.py
@@ -1,50 +1,61 @@
 import permute
-inp1 = ['a','b','ac','abcd','abcde']
-total = len(inp1)
-domain = 5
-initial = [-1]*domain
-i,j = 0,0
-final = [-1]*domain
-for index in range(total):
-    if initial[len(inp1[index])-1] == -1:
-        initial[len(inp1[index])-1] = index
-        final[len(inp1[index])-1] = index
-    else :
-        final[len(inp1[index])-1] = index    
-#print("INITIAL")
-#print(initial)
-#print("FINAL")
-#print(final)
 
-for index in range(total-1):
-    if initial[len(inp1[index])] == -1:
-        index2 = initial[len(inp1[index])+1]
-        #permute.permute(inp1[index],inp1[index2],comparison)
-        #print("-1 encountered") #input data here itself else blunder!!!!
-    else:
-        index2 = initial[len(inp1[index])]
-        while index2 <= final[len(inp1[index])]:
-            comparison = [0]*256
-            for check in range(len(inp1[index])):
-                if comparison[ord(inp1[index][check])] == 0:
-                    comparison[ord(inp1[index][check])] = 1
-                elif comparison[ord(inp1[index][check])] == 1:
-                    comparison[ord(inp1[index][check])] = 0
-            for check in range(len(inp1[index2])):
-                #print(inp1[index2])
-                if comparison[ord(inp1[index2][check])] == 0:
-                    comparison[ord(inp1[index2][check])] = 1
-                elif comparison[ord(inp1[index2][check])] == 1:
-                    comparison[ord(inp1[index2][check])] = 0
-            diff = 0
-            for ini in range(256):
-                diff = diff + comparison[ini]
-            #print(diff)
-            if( diff > 1):
-                permute.permute(inp1[index],inp1[index2],comparison)
-                #print("Permute Values") # Permute values here also  
-            #print(comparison)
-            #break
-            index2 = index2+1;
-    #break
-            
+def query(inp1,domain):
+    total = len(inp1)
+    initial = [-1]*domain
+    i,j = 0,0
+    final = [-1]*domain
+    for index in range(total):
+        if initial[len(inp1[index])-1] == -1:
+            initial[len(inp1[index])-1] = index
+            final[len(inp1[index])-1] = index
+        else :
+            final[len(inp1[index])-1] = index    
+    print("INITIAL")
+    print(initial)
+    print("FINAL")
+    print(final)
+    print("Input:")
+    print(inp1)
+    for index in range(total-1):
+        if initial[len(inp1[index])] == -1:
+            index2 = initial[len(inp1[index])+1]
+            #permute.permute(inp1[index],inp1[index2],comparison)
+            #print("-1 encountered") #input data here itself else blunder!!!!
+        else:
+            index2 = initial[len(inp1[index])]
+            while index2 <= final[len(inp1[index])]:
+                comparison = [0]*256
+                for check in range(len(inp1[index])):
+                    if comparison[ord(inp1[index][check])] == 0:
+                        comparison[ord(inp1[index][check])] = 1
+                    elif comparison[ord(inp1[index][check])] == 1:
+                        comparison[ord(inp1[index][check])] = 0
+                for check in range(len(inp1[index2])):
+                    #print(inp1[index2])
+                    if comparison[ord(inp1[index2][check])] == 0:
+                        comparison[ord(inp1[index2][check])] = 1
+                    elif comparison[ord(inp1[index2][check])] == 1:
+                        comparison[ord(inp1[index2][check])] = 0
+                diff = 0
+                for ini in range(256):
+                    diff = diff + comparison[ini]
+                #print(diff)
+                if( diff > 1):
+                    combi = permute.permute(inp1[index],inp1[index2],comparison)
+                    count = len(inp1) 
+                    for x in combi:
+                        if x not in inp1:
+                            inp1.append(x)
+                    inp1 = sorted(inp1, key=lambda x: len(x))
+                    count2 = len(inp1)
+                    if(count2 > count):
+                        query(inp1,domain)
+                #break
+                index2 = index2+1;
+    return
+
+inp1 = ['a','b','ac','abcd','abcde']
+domain = 5
+query(inp1,domain)
+                
