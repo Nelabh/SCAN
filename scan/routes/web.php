841a13098052ca8b590257d515a11dc007aae600
Sushumna Khandelwal <sush.94kh@gmail.com>
2017-04-20 23:29:19 +0530

    question ui

--- scan/routes/web.php
+++ scan/routes/web.php
@@ -18,6 +18,8 @@ Route::group(['middleware' => 'auth'], function () {
 });
 
 Route::get('/',array('as'=>'login','uses'=>'PagesController@home'));
+Route::get('question',array('as'=>'question','uses'=>'PagesController@question'));
+
 Route::get('logout',array('as'=>'logout','uses'=>'PagesController@logout'));
 Route::post('log',array('as'=>'log','uses'=>'PagesController@log'));
 
